4ed6a5b040de1ddafe5a78fe4e12b767c32b511d
Jae Paek <jaepaek@amazon.com>
2020-11-23 16:11:48 -0800

    Renaming the ViewController name and creating a new controller for the photo album

---
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by Paek, Jae on 11/23/20.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+}
